60eced9f83af888741049a2d6368d810cee1be3f
rakib <hassanrakibul926@gmail.com>
2024-02-20 13:15:56 +0600

    user prifle and my order section toggle apply

--- Furnitouch/static/js/my-order.js
+++ Furnitouch/static/js/my-order.js
@@ -1,3 +1,22 @@
-document.getElementById('test_ID').addEventListener('click', ()=>{
-    console.log('Hello Bangladesh')
+document.getElementById('userProfileHide').addEventListener('click', function(){
+    console.log('Hello')
+    this.style.color = '#F39C11'
+    document.getElementById('myOrderHidden').style.color = 'black'
+    const profile = document.getElementById('profileHidden')
+    profile.classList.remove('d-none')
+    const myOrder = document.getElementById('OrderHidden')
+    myOrder.classList.add('d-none')
+
+
+});
+
+document.getElementById('myOrderHidden').addEventListener('click', function(){
+    console.log('Hello')
+    this.style.color = '#F39C11'
+    document.getElementById('userProfileHide').style.color = 'black'
+    const myOrder = document.getElementById('OrderHidden')
+    myOrder.classList.remove('d-none')
+
+    const profile = document.getElementById('profileHidden')
+    profile.classList.add('d-none')
 })
