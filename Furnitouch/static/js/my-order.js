eca4e2cfc277200ae484350582d71acc39aec8f2
rakib <hassanrakibul926@gmail.com>
2024-02-20 13:44:05 +0600

    my delivery order  section toggle apply design

--- Furnitouch/static/js/my-order.js
+++ Furnitouch/static/js/my-order.js
@@ -2,10 +2,12 @@ document.getElementById('userProfileHide').addEventListener('click', function(){
     console.log('Hello')
     this.style.color = '#F39C11'
     document.getElementById('myOrderHidden').style.color = 'black'
+    document.getElementById('myDeliveredOrderHidden').style.color = 'black'
     const profile = document.getElementById('profileHidden')
     profile.classList.remove('d-none')
     const myOrder = document.getElementById('OrderHidden')
     myOrder.classList.add('d-none')
+    document.getElementById('DeliveredOrderHidden').classList.add('d-none')
 
 
 });
@@ -14,9 +16,28 @@ document.getElementById('myOrderHidden').addEventListener('click', function(){
     console.log('Hello')
     this.style.color = '#F39C11'
     document.getElementById('userProfileHide').style.color = 'black'
+    document.getElementById('myDeliveredOrderHidden').style.color = 'black'
     const myOrder = document.getElementById('OrderHidden')
     myOrder.classList.remove('d-none')
+    
 
     const profile = document.getElementById('profileHidden')
     profile.classList.add('d-none')
+    document.getElementById('DeliveredOrderHidden').classList.add('d-none')
+
+})
+
+document.getElementById('myDeliveredOrderHidden').addEventListener('click', function(){
+    document.getElementById('myOrderHidden').style.color = 'black'
+    document.getElementById('userProfileHide').style.color = 'black'
+    this.style.color = '#F39C11'
+    const deliveredOrder = document.getElementById('DeliveredOrderHidden')
+    deliveredOrder.classList.remove('d-none')
+    const myOrder = document.getElementById('OrderHidden')
+    myOrder.classList.add('d-none')
+
+    const profile = document.getElementById('profileHidden')
+    profile.classList.add('d-none')
+    
+    
 })
